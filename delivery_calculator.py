004dd8b7f91138a2b9eed0649f77e768d1a9204e
WassawRoki <56611129+WassawRoki@users.noreply.github.com>
2023-07-13 00:41:21 +0200

    missing: add special non workdays

--- delivery_calculator.py
+++ delivery_calculator.py
@@ -1,32 +1,40 @@
-from datetime import datetime,timedelta
+from datetime import datetime,timedelta,timezone
 import holidays
 
 def get_delivery_date(date : datetime):
+    'From an input date finds the expected delivery date'
     
     #dictionary of danish holidays
     dk_holidays = holidays.country_holidays('DK')
     
     expected_delivery_date : datetime = date
-    current_datetime = datetime.now()
+    temp_date : datetime = date
     
-    days_added : int = 0
-    tempdays = 0
+    work_days : int =0
     #we want to add 2 workdays to the delivery time.
-    while days_added < 2:
-        tempdays = tempdays + timedelta(days=1)
+    days_since_order: int = 0
+    
+    
+    while work_days < 2:
         #first we check if the date is not a weekend or a holiday
-        if date.hour < 15:
-            if date + timedelta(days=1)+tempdays not in dk_holidays and (date + timedelta(days=1)+tempdays).weekday() < 5:
-                days_added +=2
+        temp_date = date + timedelta(days=days_since_order+1)
+        if temp_date.hour < 15:
+            if temp_date not in dk_holidays and temp_date.weekday() < 5:
+                days_since_order +=1
                 expected_delivery_date += timedelta(days=1)
+                work_days = work_days+2
             else:
                 expected_delivery_date += timedelta(days=1)
+                days_since_order +=1
         else:
-            if date + timedelta(days=1) + expected_delivery_date not in dk_holidays and (date + timedelta(days=1)+ expected_delivery_date).weekday() < 5:
-                days_added +=1
-                expected_delivery_date += timedelta(days=1)
+            if temp_date not in dk_holidays and temp_date.weekday() < 5:
+                days_since_order +=1
+                work_days = work_days+1
             else:
-                expected_delivery_date += timedelta(days=1)
-        
+                days_since_order +=1
+
+
     
     return expected_delivery_date.strftime("%Y, %m, %d")
+
+get_delivery_date(datetime(2020, 12, 29, 12, 15, 12, 0, tzinfo=timezone.utc))
--- pytests/test_calculate_delivery_day.py
+++ pytests/test_calculate_delivery_day.py
@@ -3,14 +3,22 @@ from delivery_calculator import get_delivery_date
 
 def test_get_delivery_date():
     from datetime import datetime, timezone
+    #test: 1
     expected = "2021, 05, 21"
-    
     value = get_delivery_date(datetime(2021, 5, 20, 12, 51, 32, 199883, tzinfo=timezone.utc))
-    
     assert value == expected
 
-    assert get_delivery_date(datetime(2021, 5, 21, 13, 3, 31, 245381, tzinfo=timezone.utc)) == "2021, 05, 25"
+    #test: 2
+    expected = "2021, 05, 25"
+    value = get_delivery_date(datetime(2021, 5, 21, 13, 3, 31, 245381, tzinfo=timezone.utc))
+    assert value == expected
 
-    assert get_delivery_date(datetime(2020, 12, 29, 12, 15, 12, 0, tzinfo=timezone.utc)) == "2020, 12, 30"
-
-    assert get_delivery_date(datetime(2020, 12, 29, 14, 15, 12, 0, tzinfo=timezone.utc)) == "2021, 01, 04"
+    #test: 3
+    expected = "2020, 12, 30"
+    value = get_delivery_date(datetime(2020, 12, 29, 12, 15, 12, 0, tzinfo=timezone.utc))
+    assert value == expected
+    
+    #test: 4
+    expected = "2022, 01, 04"
+    value = get_delivery_date(datetime(2021, 12, 30, 14, 15, 12, 0, tzinfo=timezone.utc))
+    assert value == expected
